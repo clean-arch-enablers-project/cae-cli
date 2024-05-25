18942ca4a52d82b006fdf0fce6761d00af02ddf9
vinicius123131 <vini989073599@gmail.com>
2024-05-25 18:30:05 -0300

    feat: atualizacoes das funcoes

--- cae/Run.py
+++ cae/Run.py
@@ -3,9 +3,12 @@ import os
 from .ArchFlowJavaWeb import ArchFlowJavaWeb
 
 
-def main():
+def main(roots_path_json=None):
+    if roots_path_json is None:
+        roots_path_json = []
+    roots_path_json.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "db.json")))
     directory_template = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
     arch = ArchFlowJavaWeb()
     arch.DirectoryExplorer.directory_template = directory_template
     args = arch.handle_args()
-    arch.handler_functions_flow_java(args)
+    arch.handler_functions_flow(args, roots_path_json)
