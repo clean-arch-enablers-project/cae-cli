9e9bbf5f917926826b44a641131cb026c41e94cd
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-21 18:58:08 -0300

    fix: export variables need to be set manually on zshrc profile

--- installer/cae-cli-macos-installer.sh
+++ installer/cae-cli-macos-installer.sh
@@ -5,6 +5,7 @@ if [ -d "$HOME/cae" ]; then
 else
   echo "Not found cae directory. Creating it."
   mkdir -p "$HOME/cae"
+  mkdir -p "$HOME/cae/file-templates"
 fi
 
 echo "#!/bin/bash" > "$HOME/cae/cae.sh"
@@ -12,19 +13,19 @@ echo "java -jar \$HOME/cae/cae-cli.jar \"\$@\"" >> "$HOME/cae/cae.sh"
 chmod +x "$HOME/cae/cae.sh"
 
 export CAE_CLI_HOME="$HOME/cae"
-export CAE_META_STRUCTURE_TEMPLATES_PATH="$HOME/cae/file-templates"
 
 if [ -f "$HOME/.zshrc" ]; then
   echo "alias cae='$HOME/cae/cae.sh'" >> "$HOME/.zshrc"
-  echo "export CAE_META_STRUCTURE_TEMPLATES_PATH=\"\$CAE_META_STRUCTURE_TEMPLATES_PATH\"" >> "$HOME/.zshrc"
   source "$HOME/.zshrc"
 elif [ -f "$HOME/.bash_profile" ]; then
   echo "alias cae='$HOME/cae/cae.sh'" >> "$HOME/.bash_profile"
-  echo "export CAE_META_STRUCTURE_TEMPLATES_PATH=\"\$CAE_META_STRUCTURE_TEMPLATES_PATH\"" >> "$HOME/.bash_profile"
   source "$HOME/.bash_profile"
 fi
 
 cp ./components/cae-cli.jar "$HOME/cae"
 cp -r ./components/file-templates "$HOME/cae/file-templates"
 
+echo "Before use cae-clie, in your .zshrc/.bash_profile file, please add the following lines:"
+echo "export CAE_CLI_HOME='$HOME/cae'"
+echo "export CAE_META_STRUCTURE_TEMPLATES_PATH='$HOME/cae/file-templates'"
 echo "Installation completed. Please restart your terminal with 'source ~/.zshrc' or 'source ~/.bash_profile'."
