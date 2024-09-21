597697340b4ea0f93a04b53da06f766b6d92df1a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-21 17:50:55 -0300

    feat: cae installer for darwin/macOS plataform

---
+++ installer/cae-cli-macos-installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -d "$HOME/cae" ]; then
+  echo "Found the cae directory. Using it."
+else
+  echo "Not found cae directory. Creating it."
+  mkdir -p "$HOME/cae"
+fi
+
+echo "#!/bin/bash" > "$HOME/cae/cae.sh"
+echo "java -jar \$HOME/cae/cae-cli.jar \"\$@\"" >> "$HOME/cae/cae.sh"
+chmod +x "$HOME/cae/cae.sh"
+
+export CAE_CLI_HOME="$HOME/cae"
+export CAE_META_STRUCTURE_TEMPLATES_PATH="$HOME/cae/file-templates"
+
+if [ -f "$HOME/.zshrc" ]; then
+  echo "alias cae='$HOME/cae/cae.sh'" >> "$HOME/.zshrc"
+  echo "export CAE_META_STRUCTURE_TEMPLATES_PATH=\"\$CAE_META_STRUCTURE_TEMPLATES_PATH\"" >> "$HOME/.zshrc"
+  source "$HOME/.zshrc"
+elif [ -f "$HOME/.bash_profile" ]; then
+  echo "alias cae='$HOME/cae/cae.sh'" >> "$HOME/.bash_profile"
+  echo "export CAE_META_STRUCTURE_TEMPLATES_PATH=\"\$CAE_META_STRUCTURE_TEMPLATES_PATH\"" >> "$HOME/.bash_profile"
+  source "$HOME/.bash_profile"
+fi
+
+cp ./components/cae-cli.jar "$HOME/cae"
+cp -r ./components/file-templates "$HOME/cae/file-templates"
+
+echo "Setup completo! Reinicie o terminal ou execute 'source ~/.zshrc' ou 'source ~/.bash_profile'."
