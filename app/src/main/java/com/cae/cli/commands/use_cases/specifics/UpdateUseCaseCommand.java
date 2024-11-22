396d998a5706a0b80a3dc324707fba9fea49005c
viniciusintech <vini989073599@gmail.com>
2024-11-21 21:37:14 -0300

    feat: criando funcao para conseguir comprar e filtrar a versao mais atual do cli

--- app/src/main/java/com/cae/cli/commands/use_cases/specifics/UpdateUseCaseCommand.java
+++ app/src/main/java/com/cae/cli/commands/use_cases/specifics/UpdateUseCaseCommand.java
@@ -10,6 +10,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class UpdateUseCaseCommand extends NewUseCaseCommand {
@@ -23,7 +25,7 @@ public class UpdateUseCaseCommand extends NewUseCaseCommand {
 
     }
 
-    public static List<String> fetchBranches(String owner, String repo) {
+    private static List<String> fetchBranches(String owner, String repo) {
         List<String> branches = new ArrayList<>();
         OkHttpClient client = new OkHttpClient();
         String url = String.format("https://api.github.com/repos/%s/%s/branches", owner, repo);
@@ -52,10 +54,43 @@ public class UpdateUseCaseCommand extends NewUseCaseCommand {
         return branches;
     }
 
+    private static String lastestVersionBranch(List<String> branches){
+        Pattern versionPattern = Pattern.compile("v(\\d+\\.\\d+\\.\\d+)");
+
+        List<String> versionBranches = new ArrayList<>();
+        for (String branch : branches) {
+            Matcher matcher = versionPattern.matcher(branch);
+            if (matcher.find()) {
+                versionBranches.add(matcher.group(1));
+            }
+        }
+        if (versionBranches.isEmpty()){
+            return null;
+        }
+        return versionBranches.stream()
+                .max(UpdateUseCaseCommand::compareVersions)
+                .orElse(null);
+    }
+
+    private static int compareVersions(String v1, String v2) {
+        String[] parts1 = v1.split("\\.");
+        String[] parts2 = v2.split("\\.");
+        for (int i = 0; i < 3; i++) {
+            int part1 = Integer.parseInt(parts1[i]);
+            int part2 = Integer.parseInt(parts2[i]);
+            if (part1 != part2) {
+                return Integer.compare(part1, part2);
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
-        String owner = "clean-arch-enablers-project";
-        String repo = "cae-cli";
+        String owner = "ViniciusInTech";
+        String repo = "clean-arch-enablers-CLI";
         List<String> branches = fetchBranches(owner, repo);
         System.out.println("Branches disponíveis: " + branches);
+        var branch = lastestVersionBranch(branches);
+        System.out.println("versão mais recente " + branch);
     }
 }
